5a292c8eedfb73f02e9b1b50451e174f5a9d20a3
James Healy <jimmy@deefa.com>
2012-12-03 18:15:21 +0100

    move FTPServer into a dedicated file
    * is 1-class per file the go norm? In any case, until I know better it's
      my preference

---
+++ ftpserver.go
@@ -0,0 +1,53 @@
+package raval
+
+import (
+	"bufio"
+	"net"
+)
+
+type FTPServer struct {
+	name        string
+	listener    *net.TCPListener
+	connections *Array
+}
+
+func NewFTPServer(listener *net.TCPListener) *FTPServer {
+	s := new(FTPServer)
+	s.name = "Go FTP Server"
+	s.listener = listener
+	s.connections = new(Array)
+	return s
+}
+
+func (ftpServer *FTPServer) Listen() (err error) {
+	for {
+		ftpConn, err := ftpServer.Accept()
+		if err != nil {
+			break
+		}
+		ftpServer.connections.Append(ftpConn)
+		terminated := make(chan bool)
+		go ftpConn.Serve(terminated)
+		<-terminated
+		ftpServer.connections.Remove(ftpConn)
+		ftpConn.Close()
+	}
+	return
+}
+
+func (ftpServer *FTPServer) Accept() (ftpConn *FTPConn, err error) {
+	tcpConn, err := ftpServer.listener.AcceptTCP()
+	if err == nil {
+		controlReader := bufio.NewReader(tcpConn)
+		controlWriter := bufio.NewWriter(tcpConn)
+		ftpConn = &FTPConn{
+			rootDir,
+			tcpConn,
+			controlReader,
+			controlWriter,
+			nil,
+		}
+	}
+	return
+}
+
--- main.go
+++ main.go
@@ -62,43 +62,6 @@ func (a *Array) Remove(object interface{}) (result bool) {
 	return
 }
 
-type FTPServer struct {
-	name        string
-	listener    *net.TCPListener
-	connections *Array
-}
-
-func (ftpServer *FTPServer) Listen() (err error) {
-	for {
-		ftpConn, err := ftpServer.Accept()
-		if err != nil {
-			break
-		}
-		ftpServer.connections.Append(ftpConn)
-		terminated := make(chan bool)
-		go ftpConn.Serve(terminated)
-		<-terminated
-		ftpServer.connections.Remove(ftpConn)
-		ftpConn.Close()
-	}
-	return
-}
-
-func (ftpServer *FTPServer) Accept() (ftpConn *FTPConn, err error) {
-	tcpConn, err := ftpServer.listener.AcceptTCP()
-	if err == nil {
-		controlReader := bufio.NewReader(tcpConn)
-		controlWriter := bufio.NewWriter(tcpConn)
-		ftpConn = &FTPConn{
-			rootDir,
-			tcpConn,
-			controlReader,
-			controlWriter,
-			nil,
-		}
-	}
-	return
-}
 
 type FTPConn struct {
 	cwd           string
