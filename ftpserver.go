aa405568b3367b3a54fd747a6fd27bfc37ea1b09
James Healy <jimmy@deefa.com>
2012-12-03 18:58:51 +0100

    remove Array trickery in main.go
    * I don't really get it, best to stick to regular slices for now

--- ftpserver.go
+++ ftpserver.go
@@ -7,14 +7,13 @@ import (
 type FTPServer struct {
 	name        string
 	listener    *net.TCPListener
-	connections *Array
+	connections []*FTPConn
 }
 
 func NewFTPServer(listener *net.TCPListener) *FTPServer {
 	s := new(FTPServer)
 	s.name = "Go FTP Server"
 	s.listener = listener
-	s.connections = new(Array)
 	return s
 }
 
@@ -24,14 +23,30 @@ func (ftpServer *FTPServer) Listen() (err error) {
 		if err != nil {
 			break
 		}
-		ftpServer.connections.Append(ftpConn)
+		ftpServer.connections = append(ftpServer.connections, ftpConn)
 		terminated := make(chan bool)
 		go ftpConn.Serve(terminated)
 		<-terminated
-		ftpServer.connections.Remove(ftpConn)
+		ftpServer.removeConnection(ftpConn)
 		ftpConn.Close()
 	}
 	return
+}
+
+func (ftpServer *FTPServer) removeConnection(ftpConn *FTPConn) {
+	i := ftpServer.indexOfConnection(ftpConn)
+	ftpServer.connections[i] = ftpServer.connections[len(ftpServer.connections)-1]
+	ftpServer.connections = ftpServer.connections[0:len(ftpServer.connections)-1]
+	return
+}
+
+func (ftpServer *FTPServer) indexOfConnection(ftpConn *FTPConn) int {
+	for p, v := range ftpServer.connections {
+		if (v == ftpConn) {
+			return p
+		}
+	}
+	return -1
 }
 
 func (ftpServer *FTPServer) Accept() (ftpConn *FTPConn, err error) {
