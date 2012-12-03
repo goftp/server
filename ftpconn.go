56982e9873365d6a1713fcf0c5c94467f6e986dc
James Healy <jimmy@deefa.com>
2012-12-03 18:18:01 +0100

    move FTPConn into its own file

---
+++ ftpconn.go
@@ -0,0 +1,84 @@
+package raval
+
+import (
+	"fmt"
+	"log"
+	"bufio"
+	"net"
+	"strings"
+)
+
+type FTPConn struct {
+	cwd           string
+	control       *net.TCPConn
+	controlReader *bufio.Reader
+	controlWriter *bufio.Writer
+	data          *net.TCPConn
+}
+
+func (ftpConn *FTPConn) WriteMessage(messageFormat string, v ...interface{}) (wrote int, err error) {
+	message := fmt.Sprintf(messageFormat, v...)
+	wrote, err = ftpConn.controlWriter.WriteString(message)
+	ftpConn.controlWriter.Flush()
+	log.Print(message)
+	return
+}
+
+func (ftpConn *FTPConn) Serve(terminated chan bool) {
+	log.Print("Connection Established")
+	// send welcome
+	ftpConn.WriteMessage(getMessageFormat(220), welcomeMessage)
+	// read commands
+	for {
+		line, err := ftpConn.controlReader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		log.Print(line)
+		params := strings.Split(strings.Trim(line, "\r\n"), " ")
+		count := len(params)
+		if count > 0 {
+			command := params[0]
+			switch command {
+			case USER:
+				ftpConn.WriteMessage(getMessageFormat(331), "User name ok, password required")
+				break
+			case PASS:
+				ftpConn.WriteMessage(getMessageFormat(230), "Password ok, continue")
+				break
+			default:
+				ftpConn.WriteMessage(getMessageFormat(500), "Command not found")
+			}
+		} else {
+			ftpConn.WriteMessage(getMessageFormat(500), "Syntax error, zero parameters")
+		}
+	}
+	terminated <- true
+	log.Print("Connection Terminated")
+}
+
+func (ftpConn *FTPConn) Close() {
+	ftpConn.control.Close()
+	if ftpConn.data != nil {
+		ftpConn.data.Close()
+	}
+}
+
+func getMessageFormat(command int) (messageFormat string) {
+	switch command {
+	case 220:
+		messageFormat = "220 %s"
+		break
+	case 230:
+		messageFormat = "230 %s"
+		break
+	case 331:
+		messageFormat = "331 %s"
+		break
+	case 500:
+		messageFormat = "500 %s"
+		break
+	}
+	return messageFormat + "\r\n"
+}
+
--- main.go
+++ main.go
@@ -1,37 +1,11 @@
 package raval
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
-	"strings"
-)
-
 const (
 	rootDir        = "/"
 	welcomeMessage = "Welcome to the Go FTP Server"
 	USER           = "USER"
 	PASS           = "PASS"
 )
-
-func getMessageFormat(command int) (messageFormat string) {
-	switch command {
-	case 220:
-		messageFormat = "220 %s"
-		break
-	case 230:
-		messageFormat = "230 %s"
-		break
-	case 331:
-		messageFormat = "331 %s"
-		break
-	case 500:
-		messageFormat = "500 %s"
-		break
-	}
-	return messageFormat + "\r\n"
-}
 
 type Array struct {
 	container []interface{}
@@ -61,61 +35,3 @@ func (a *Array) Remove(object interface{}) (result bool) {
 	}
 	return
 }
-
-
-type FTPConn struct {
-	cwd           string
-	control       *net.TCPConn
-	controlReader *bufio.Reader
-	controlWriter *bufio.Writer
-	data          *net.TCPConn
-}
-
-func (ftpConn *FTPConn) WriteMessage(messageFormat string, v ...interface{}) (wrote int, err error) {
-	message := fmt.Sprintf(messageFormat, v...)
-	wrote, err = ftpConn.controlWriter.WriteString(message)
-	ftpConn.controlWriter.Flush()
-	log.Print(message)
-	return
-}
-
-func (ftpConn *FTPConn) Serve(terminated chan bool) {
-	log.Print("Connection Established")
-	// send welcome
-	ftpConn.WriteMessage(getMessageFormat(220), welcomeMessage)
-	// read commands
-	for {
-		line, err := ftpConn.controlReader.ReadString('\n')
-		if err != nil {
-			break
-		}
-		log.Print(line)
-		params := strings.Split(strings.Trim(line, "\r\n"), " ")
-		count := len(params)
-		if count > 0 {
-			command := params[0]
-			switch command {
-			case USER:
-				ftpConn.WriteMessage(getMessageFormat(331), "User name ok, password required")
-				break
-			case PASS:
-				ftpConn.WriteMessage(getMessageFormat(230), "Password ok, continue")
-				break
-			default:
-				ftpConn.WriteMessage(getMessageFormat(500), "Command not found")
-			}
-		} else {
-			ftpConn.WriteMessage(getMessageFormat(500), "Syntax error, zero parameters")
-		}
-	}
-	terminated <- true
-	log.Print("Connection Terminated")
-}
-
-func (ftpConn *FTPConn) Close() {
-	ftpConn.control.Close()
-	if ftpConn.data != nil {
-		ftpConn.data.Close()
-	}
-}
-
