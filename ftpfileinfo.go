24b0983ce0c95992fbb30cad4bf8415ac75bbe9b
James Healy <jimmy@deefa.com>
2012-12-13 18:41:32 +0100

    the ftpFileInfo type should not be exported from the package
    * a small API is a stable API

--- ftpfileinfo.go
+++ ftpfileinfo.go
@@ -5,52 +5,52 @@ import (
 	"time"
 )
 
-type FTPFileInfo struct {
+type ftpFileInfo struct {
 	name      string
 	bytes     int64
 	mode      os.FileMode
 }
 
-func (info *FTPFileInfo) Name() string {
+func (info *ftpFileInfo) Name() string {
 	return info.name
 }
 
-func (info *FTPFileInfo) Size() int64 {
+func (info *ftpFileInfo) Size() int64 {
 	return info.bytes
 }
 
-func (info *FTPFileInfo) Mode() os.FileMode {
+func (info *ftpFileInfo) Mode() os.FileMode {
 	return info.mode
 }
 
-func (info *FTPFileInfo) ModTime() time.Time {
+func (info *ftpFileInfo) ModTime() time.Time {
 	return time.Now()
 }
 
-func (info *FTPFileInfo) IsDir() bool {
+func (info *ftpFileInfo) IsDir() bool {
 	return (info.mode | os.ModeDir) == os.ModeDir
 }
 
-func (info *FTPFileInfo) Sys() interface{} {
+func (info *ftpFileInfo) Sys() interface{} {
 	return nil
 }
 
-// NewDirItem creates a new FTPFileInfo that represents a single diretory. Use
+// NewDirItem creates a new ftpFileInfo that represents a single diretory. Use
 // this function to build the response to DirContents() in your FTPDriver
 // implementation.
 func NewDirItem(name string) os.FileInfo {
-	d := new(FTPFileInfo)
+	d := new(ftpFileInfo)
 	d.name = name
 	d.bytes = int64(0)
 	d.mode = os.ModeDir | 666
 	return d
 }
 
-// NewFileItem creates a new FTPFileInfo that represents a single file. Use
+// NewFileItem creates a new ftpFileInfo that represents a single file. Use
 // this function to build the response to DirContents() in your FTPDriver
 // implementation.
 func NewFileItem(name string, bytes int) os.FileInfo {
-	f := new(FTPFileInfo)
+	f := new(ftpFileInfo)
 	f.name = name
 	f.bytes = int64(bytes)
 	f.mode = 666
