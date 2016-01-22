0179d3028a001f37e05c02e085576671459e7490
Lunny Xiao <xiaolunwen@gmail.com>
2016-01-22 22:11:08 +0800

    bug fixed for list with tags

--- cmd.go
+++ cmd.go
@@ -343,10 +343,21 @@ func (cmd commandList) RequireAuth() bool {
 
 func (cmd commandList) Execute(conn *Conn, param string) {
 	conn.writeMessage(150, "Opening ASCII mode data connection for file list")
-	fpath := param
-	if param == "-l" {
-		fpath = conn.namePrefix
+	var fpath string
+	if len(param) == 0 {
+		fpath = param
+	} else {
+		fields := strings.Fields(param)
+		for _, field := range fields {
+			if strings.HasPrefix(field, "-") {
+				//TODO: currently ignore all the flag
+				fpath = conn.namePrefix
+			} else {
+				fpath = field
+			}
+		}
 	}
+
 	path := conn.buildPath(fpath)
 	info, err := conn.driver.Stat(path)
 	if err != nil {
@@ -387,9 +398,34 @@ func (cmd commandNlst) RequireAuth() bool {
 
 func (cmd commandNlst) Execute(conn *Conn, param string) {
 	conn.writeMessage(150, "Opening ASCII mode data connection for file list")
-	path := conn.buildPath(param)
+	var fpath string
+	if len(param) == 0 {
+		fpath = param
+	} else {
+		fields := strings.Fields(param)
+		for _, field := range fields {
+			if strings.HasPrefix(field, "-") {
+				//TODO: currently ignore all the flag
+				fpath = conn.namePrefix
+			} else {
+				fpath = field
+			}
+		}
+	}
+
+	path := conn.buildPath(fpath)
+	info, err := conn.driver.Stat(path)
+	if err != nil {
+		conn.writeMessage(550, err.Error())
+		return
+	}
+	if !info.IsDir() {
+		// TODO: should we show the file description?
+		return
+	}
+
 	var files []FileInfo
-	err := conn.driver.ListDir(path, func(f FileInfo) error {
+	err = conn.driver.ListDir(path, func(f FileInfo) error {
 		files = append(files, f)
 		return nil
 	})
