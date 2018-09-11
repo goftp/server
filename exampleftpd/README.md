ceca2dad1e7420192f8603b657ef669ab8b73ce2
Nick Craig-Wood <nick@craig-wood.com>
2018-09-11 06:18:22 +0100

    Make a new simpler example server (#52)
    Before this the example server was goftp/ftpd which is a good server
    but no longer a simple example.  Replace it with a very simple example
    which uses the file-driver to serve files from the disk.

---
+++ exampleftpd/README.md
@@ -0,0 +1,20 @@
+# exampleftpd
+
+This is a simple example ftpd server for testing against and to demonstrate how to use the interface.
+
+Change to this directory then build it with `go build`
+
+```
+$ ./exampleftpd -h
+Usage of ./exampleftpd:
+  -host string
+    	Port (default "localhost")
+  -pass string
+    	Password for login (default "123456")
+  -port int
+    	Port (default 2121)
+  -root string
+    	Root directory to serve
+  -user string
+    	Username for login (default "admin")
+```
---
+++ exampleftpd/exampleftpd.go
@@ -0,0 +1,49 @@
+// Copyright 2018 The goftp Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// This is a very simple ftpd server using this library as an example
+// and as something to run tests against.
+package main
+
+import (
+	"flag"
+	"log"
+
+	filedriver "github.com/goftp/file-driver"
+	"github.com/goftp/server"
+)
+
+func main() {
+	var (
+		root = flag.String("root", "", "Root directory to serve")
+		user = flag.String("user", "admin", "Username for login")
+		pass = flag.String("pass", "123456", "Password for login")
+		port = flag.Int("port", 2121, "Port")
+		host = flag.String("host", "localhost", "Port")
+	)
+	flag.Parse()
+	if *root == "" {
+		log.Fatalf("Please set a root to serve with -root")
+	}
+
+	factory := &filedriver.FileDriverFactory{
+		RootPath: *root,
+		Perm:     server.NewSimplePerm("user", "group"),
+	}
+
+	opts := &server.ServerOpts{
+		Factory:  factory,
+		Port:     *port,
+		Hostname: *host,
+		Auth:     &server.SimpleAuth{Name: *user, Password: *pass},
+	}
+
+	log.Printf("Starting ftp server on %v:%v", opts.Hostname, opts.Port)
+	log.Printf("Username %v, Password %v", *user, *pass)
+	server := server.NewServer(opts)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+	}
+}
